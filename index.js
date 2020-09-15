87713e82ad86288cf56c4bb01922da6801c70635
yeugen1 <yeugen@gmail.com>
2020-09-15 18:32:03 +0200

    added symbol

--- index.js
+++ index.js
@@ -83,3 +83,21 @@ text.search(pattern)           // => 9: position of first match
 text.match(pattern)            // => ["1", "2", "3"]: array of all matches
 text.replace(pattern, "#")     // => "testing: #, #, #"
 text.split(/\D+/)              // => ["","1","2","3"]: split on nondigits
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Symbols
+let strname = "string name";      // A string to use as a property name
+let symname = Symbol("propname"); // A Symbol to use as a property name
+typeof strname                    // => "string": strname is a string
+typeof symname                    // => "symbol": symname is a symbol
+let o = {};                       // Create a new object
+o[strname] = 1;                   // Define a property with a string name
+o[symname] = 2;                   // Define a property with a Symbol name
+o[strname]                        // => 1: access the string-named property
+o[symname]                        // => 2: access the symbol-named property
+let s = Symbol.for("shared");
+let t = Symbol.for("shared");
+s === t                           // => true
+s.toString()                      // => "Symbol(shared)"
+Symbol.keyFor(t)                  // => "shared"
